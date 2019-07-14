52e1ac9acb492edd1c458aba3348b3118d66a96c
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2019-07-14 14:08:41 +0300

    setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
+
+import { Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { createCustomElement } from '@angular/elements';
 // import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-import { AlertComponent } from './alert.component';
-import { Injector, NgModule } from '@angular/core';
-import { createCustomElement } from '@angular/elements';
-import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { AppComponent } from './app.component';
+import { AlertComponent } from './alert.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,7 @@ import { AppComponent } from './app.component';
     BrowserModule
   ],
   // schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
-  entryComponents: [AlertComponent, HelloWorldComponent],
+  entryComponents: [AlertComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {
