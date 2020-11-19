307effe082eb679a447a35276f5499ebb5a622c8
stepanenko <serhiy.stepanenko@aiesec.net>
2020-11-20 01:37:29 +0200

    simplified mongo connection code

--- api/index.js
+++ api/index.js
@@ -4,34 +4,14 @@ import { MongoClient } from 'mongodb';
 import assert from 'assert';
 import config from '../config';
 
-const mongoClient = new MongoClient(config.mongodbUri, { useUnifiedTopology: true });
 let database;
-mongoClient.connect()
-  .then(() => {
+MongoClient.connect(config.mongodbUri, { useUnifiedTopology: true })
+  .then((mongoClient) => {
     database = mongoClient.db('react_full');
     console.log('Connected to MongoDB');
   })
-  .catch(err => console.error('Couldn\'t connect to DB:', err));
+  .catch(err => console.error('Couldn\'t connect to DB:', err))
 
-
-//// Function to connect to the server
-async function run() {
-  try {
-    // Connect the client to the server
-    await mongoClient.connect();
-
-    // const database = mongoClient.db('react_full');
-    // const collection = database.collection('contests');
-    // Query for a contest that has the categoryName: "Website"
-    // const query = { categoryName: 'Website' };
-    // const contest = await collection.findOne(query);
-    // console.log('RESULT:', contest);
-  } finally {
-    // Ensures that the mongoClient will close when you finish/error
-    await mongoClient.close();
-  }
-}
-// run().catch(console.dir);
 
 const router = express.Router();
 
