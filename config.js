a64835e4df25521b0f274482bf152741a47aa286
stepanenko <serhiy.stepanenko@aiesec.net>
2020-11-19 21:02:42 +0200

    connected to mongo and added test data to it

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ const env = process.env;
 export default {
   port: env.PORT || 8080,
   host: env.HOST || '0.0.0.0',
+  mongodbUri: 'mongodb+srv://sergio:333444@react-samer-buna.uuh93.mongodb.net/test?retryWrites=true&w=majority',
   get serverUrl() {
     return `http://${this.host}:${this.port}`;
   }
