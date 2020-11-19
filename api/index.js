530276d38664fe8ee2cf68bb9ebd08658d582bca
stepanenko <serhiy.stepanenko@aiesec.net>
2020-11-20 01:26:43 +0200

    reading from mongo and displaying data

--- api/index.js
+++ api/index.js
@@ -1,23 +1,65 @@
 
 import express from 'express';
-import data from '../src/testData';
+import { MongoClient } from 'mongodb';
+import assert from 'assert';
+import config from '../config';
+
+const mongoClient = new MongoClient(config.mongodbUri, { useUnifiedTopology: true });
+let database;
+mongoClient.connect()
+  .then(() => {
+    database = mongoClient.db('react_full');
+    console.log('Connected to MongoDB');
+  })
+  .catch(err => console.error('Couldn\'t connect to DB:', err));
+
+
+//// Function to connect to the server
+async function run() {
+  try {
+    // Connect the client to the server
+    await mongoClient.connect();
+
+    // const database = mongoClient.db('react_full');
+    // const collection = database.collection('contests');
+    // Query for a contest that has the categoryName: "Website"
+    // const query = { categoryName: 'Website' };
+    // const contest = await collection.findOne(query);
+    // console.log('RESULT:', contest);
+  } finally {
+    // Ensures that the mongoClient will close when you finish/error
+    await mongoClient.close();
+  }
+}
+// run().catch(console.dir);
 
 const router = express.Router();
 
-const contests = data.contests.reduce((obj, contest) => {
-  obj[contest.id] = contest;
-  return obj;
-}, {});
-
 router.get('/contests', (req, res) => {
-  res.send({ contests: contests });
+  let contests = {};
+  database.collection('contests').find({})
+    .project({
+      id: 1,
+      categoryName: 1,
+      contestName: 1
+    })
+    .each((err, contest) => {
+      assert.strictEqual(null, err);
+
+      if (!contest) { // no more contests
+        res.send({ contests });
+        return;
+      }
+
+      contests[contest.id] = contest;
+    });
 });
 
 router.get('/contests/:contestId', (req, res) => {
-  let contest = contests[req.params.contestId];
-  contest.description = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-
-  res.send(contest);
+  database.collection('contests')
+    .findOne({ id: Number(req.params.contestId) })
+    .then(contest => res.send(contest))
+    .catch(console.error);
 });
 
 export default router;
--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const env = process.env;
 export default {
   port: env.PORT || 8080,
   host: env.HOST || '0.0.0.0',
-  mongodbUri: 'mongodb+srv://sergio:333444@react-samer-buna.uuh93.mongodb.net/test?retryWrites=true&w=majority',
+  mongodbUri: 'mongodb+srv://sergio:333444@react-samer-buna.uuh93.mongodb.net/?retryWrites=true&w=majority',
   get serverUrl() {
     return `http://${this.host}:${this.port}`;
   }
